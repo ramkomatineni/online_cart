a6420e4aa77d1cfd2fae722d1410711c2b31fbfc
rk <krishna.mrk503@gmail.com>
2018-01-04 22:26:20 -0500

    My project setup completed successfully

---
+++ cartbackend/src/main/java/com/rk/cartbackend/App.java
@@ -0,0 +1,13 @@
+package com.rk.cartbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
